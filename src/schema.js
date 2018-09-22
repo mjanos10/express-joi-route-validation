f3c92188ab6196b119907cb2204931bb732b71b5
janos.mayer <hq@mywebstuff.org>
2018-09-22 16:32:22 +0200

    Add schema validation for joiOptions

--- src/schema.js
+++ src/schema.js
@@ -1,8 +1,28 @@
 const Joi = require('joi')
 
 const requestPropSchema = Joi.object().keys({
-  errorMessage: Joi.alternatives().try(Joi.func(), Joi.string()),
   schema: Joi.any().required(),
+  errorMessage: Joi.alternatives().try(Joi.func(), Joi.string()),
+  joiOptions: Joi.object().keys({
+    abortEarly: Joi.boolean().default(true),
+    convert: Joi.boolean().default(true),
+    allowUnknown: Joi.boolean().default(false),
+    skipFunctions: Joi.boolean().default(false),
+    stripUnknown: Joi.alternatives().try(
+      Joi.boolean().default(false),
+      Joi.object().keys({
+        arrays: Joi.boolean().default(false),
+        objects: Joi.boolean().default(false),
+      }),
+    ),
+    languaage: Joi.any().default({}),
+    presence: Joi.string()
+      .valid('optional', 'required', 'forbidden')
+      .default('optional'),
+    context: Joi.any(),
+    noDefaults: Joi.boolean().default(false),
+    escapeHtml: Joi.boolean().default(false),
+  }),
 })
 
 const settingsSchema = Joi.object()
