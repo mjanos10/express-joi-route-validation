59361eeb12ccf84adee807ce7f3a23795a415678
janos.mayer <hq@mywebstuff.org>
2018-09-22 18:04:20 +0200

    Add validation tests for joi options

--- test/schema.test.js
+++ test/schema.test.js
@@ -48,4 +48,88 @@ describe('settingsSchema validation tests', () => {
       expect(error).toBe(null)
     })
   })
+
+  describe('joiOptions validations', () => {
+    const booleanOptions = [
+      'abortEarly',
+      'convert',
+      'allowUnknown',
+      'skipFunctions',
+      'noDefaults',
+      'escapeHtml',
+    ]
+
+    test.each(booleanOptions)('%s fails when not a boolean', prop => {
+      const obj = {
+        body: {
+          schema: {},
+          joiOptions: { [prop]: 'string' },
+        },
+      }
+      const { error } = validateSettingsSchema(obj)
+      expect(error).not.toBe(null)
+    })
+
+    test.each(booleanOptions)('%s passes test when a boolean', prop => {
+      const obj = {
+        body: {
+          schema: {},
+          joiOptions: { [prop]: false },
+        },
+      }
+      const { error } = validateSettingsSchema(obj)
+      expect(error).toBe(null)
+    })
+
+    test('stripUnknown passes when a boolean', () => {
+      const { error } = validateSettingsSchema({
+        body: {
+          schema: {},
+          joiOptions: { stripUnknown: false },
+        },
+      })
+      expect(error).toBe(null)
+    })
+
+    test('stripUnknown passes when an object', () => {
+      const { error } = validateSettingsSchema({
+        body: {
+          schema: {},
+          joiOptions: { stripUnknown: { arrays: false, objects: true } },
+        },
+      })
+      expect(error).toBe(null)
+    })
+
+    test('stripUnknown fails when an object with wrong keys', () => {
+      const { error } = validateSettingsSchema({
+        body: {
+          schema: {},
+          joiOptions: { stripUnknown: { arrays: 'non bool' } },
+        },
+      })
+      expect(error).not.toBe(null)
+    })
+
+    test('stripUnknown fails when not an object or a string', () => {
+      const { error } = validateSettingsSchema({
+        body: { schema: {}, joiOptions: { stripUnknown: 'non-bool' } },
+      })
+      expect(error).not.toBe(null)
+    })
+
+    test('presence should fails when not a valid string', () => {
+      const { error } = validateSettingsSchema({
+        body: { schema: {}, joiOptions: { presence: 'invalid' } },
+      })
+      expect(error).not.toBe(null)
+    })
+
+    test('presence should passes when a valid string', () => {
+      const { error } = validateSettingsSchema({
+        body: { schema: {}, joiOptions: { presence: 'forbidden' } },
+      })
+      expect(error).toBe(null)
+    })
+  })
 })
